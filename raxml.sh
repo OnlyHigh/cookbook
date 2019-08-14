1bb43d244bfd077b5d69de921798808b360a6b8a
Luis A. Arteaga-Figueroa <larteag7@eafit.edu.co>
2019-08-14 17:45:06 -0500

    Update raxml.sh

--- raxml.sh
+++ raxml.sh
@@ -16,7 +16,8 @@
 export SBATCH_EXPORT=NONE
 # APOLO
 #module load raxml/8.2.12_intel-17.0.1
+#para apolo -- raxmlHPC-HYBRID-AVX
 # CRONOS
-#raxml/8.2.12_intel-18.02
-#source activate Misc 
-#srun --mpi=pmi2 raxmlHPC-HYBRID-AVX2 -T $SLURM_CPUS_PER_TASK -f a -p 12345 -x 12345 -# 10000 -m GTRCAT -n whole-cp -s cp-al.fa
+#module load raxml/8.2.12_intel-18.02
+#para cronos -- raxmlHPC-HYBRID-AVX2
+srun --mpi=pmi2 raxmlHPC-HYBRID-AVX2 -T $SLURM_CPUS_PER_TASK -f a -p 12345 -x 12345 -# 10000 -m GTRCAT -n whole-cp -s cp-al.fa
