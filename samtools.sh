5067023e0c3709021c04e98a91cca7e46f01ece3
Luis A. Arteaga-Figueroa <larteag7@eafit.edu.co>
2019-08-14 18:01:08 -0500

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -15,7 +15,6 @@
 export SBATCH_EXPORT=NONE
 module load python/3.6.5_miniconda-4.5.4
 source activate Misc
-#samtools view -bS -@ 16 aln-pe-M2.sam > aln-pe-M2.bam 
-#samtools view -b -F 4 -@ 16 aln-pe-M2.bam > aln-pe-M2-mapped.bam
-#samtools sort -n -o aln-pe-M2-mapped-sortedbyname.bam aln-pe-M2-mapped.bam
-samtools fastq -1 M2-forward.fq -2 M2-reverse.fq -0 /dev/null -s /dev/null -n -F 0x900 aln-pe-M2-mapped-sortedbyname.bam
+samtools view -bS -@ 16 aln-pe-M2.sam > aln-pe-M2.bam 
+samtools sort -o aln-pe-M2-mapped-sortedbyname.bam aln-pe-M2-mapped.bam
+samtools index aln-pe-M2-mapped-sortedbyname.bam
